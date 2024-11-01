86cf067885825364eb67c2331223983c9e1acf72
Vladimir Hrusovsky <wladimiiir@gmail.com>
2024-11-01 10:07:05 +0100

    Fixed issue with plugin not starting on consecutive starts of IDE (0.1.4)

--- src/main/kotlin/me/wladimiiir/plugins/aidercompanion/AiderCompanionPluginStartupActivity.kt
+++ src/main/kotlin/me/wladimiiir/plugins/aidercompanion/AiderCompanionPluginStartupActivity.kt
@@ -1,11 +1,9 @@
 package me.wladimiiir.plugins.aidercompanion;
 
-import com.intellij.ide.util.RunOnceUtil
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
-import kotlinx.coroutines.Runnable
 
 class AiderCompanionPluginStartupActivity : StartupActivity {
     private val LOG = Logger.getInstance(AiderCompanionPluginStartupActivity::class.java)
@@ -15,7 +13,7 @@ class AiderCompanionPluginStartupActivity : StartupActivity {
             LOG.info("AiderCompanionPluginStartupActivity: Initializing AiderCompanion")
 
             val application = ApplicationManager.getApplication().getService(AiderCompanionAppService::class.java)
-            RunOnceUtil.runOnceForApp("startAiderCompanionServer", Runnable { application.startServer() })
+            application.startServer()
 
             LOG.info("AiderCompanionPluginStartupActivity: AiderCompanion initialized successfully")
         } catch (e: Exception) {
